e8bb11d9f46c93b9b3d7df1558911f71eea67382
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-05-03 20:16:01 -0400

    Init Basic Swift

---
+++ Swift/basic.swift
@@ -0,0 +1,14 @@
+print("Hello, world!")
+// ====== Data Types =======
+// https://www.tutorialspoint.com/swift/swift_data_types.htm
+
+// ====== Tuples  =======
+var error501 = (errorCode: 501, description: "Not Implemented")
+print(error501.0) // prints 501.
+print(error501.errorCode) // prints 501.
+
+// ====== Constants Declaration  =======
+let constA = 42
+print(constA)
+let constB: Float = 3.14159 
+print(constB) // a type annotation
